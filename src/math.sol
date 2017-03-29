b6d102107284506d7b81ad653ec9276bd74e727e
Nikolai Mushegian <nikolai@nexusdev.us>
2017-03-29 19:20:10 -0400

    min/max

--- src/math.sol
+++ src/math.sol
@@ -57,4 +57,12 @@ contract DSMath is DSAver {
             }
         }
     }
+
+    function min(uint128 x, uint128 y) constant internal returns (uint128 z) {
+        return (x <= y) ? x : y;
+    }
+    function max(uint128 x, uint128 y) constant internal returns (uint128 z) {
+        return (x >= y) ? x : y;
+    }
+
 }
