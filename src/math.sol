352032e863b79f823d88362248f3fd0d39112c1b
rain <rainbreak@riseup.net>
2017-10-02 12:45:32 +0100

    fix solc warnings

--- src/math.sol
+++ src/math.sol
@@ -12,42 +12,42 @@
 pragma solidity ^0.4.13;
 
 contract DSMath {
-    function add(uint x, uint y) internal returns (uint z) {
+    function add(uint x, uint y) internal pure returns (uint z) {
         require((z = x + y) >= x);
     }
-    function sub(uint x, uint y) internal returns (uint z) {
+    function sub(uint x, uint y) internal pure returns (uint z) {
         require((z = x - y) <= x);
     }
-    function mul(uint x, uint y) internal returns (uint z) {
+    function mul(uint x, uint y) internal pure returns (uint z) {
         require(y == 0 || (z = x * y) / y == x);
     }
 
-    function min(uint x, uint y) internal returns (uint z) {
+    function min(uint x, uint y) internal pure returns (uint z) {
         return x <= y ? x : y;
     }
-    function max(uint x, uint y) internal returns (uint z) {
+    function max(uint x, uint y) internal pure returns (uint z) {
         return x >= y ? x : y;
     }
-    function imin(int x, int y) internal returns (int z) {
+    function imin(int x, int y) internal pure returns (int z) {
         return x <= y ? x : y;
     }
-    function imax(int x, int y) internal returns (int z) {
+    function imax(int x, int y) internal pure returns (int z) {
         return x >= y ? x : y;
     }
 
     uint constant WAD = 10 ** 18;
     uint constant RAY = 10 ** 27;
 
-    function wmul(uint x, uint y) internal returns (uint z) {
+    function wmul(uint x, uint y) internal pure returns (uint z) {
         z = add(mul(x, y), WAD / 2) / WAD;
     }
-    function rmul(uint x, uint y) internal returns (uint z) {
+    function rmul(uint x, uint y) internal pure returns (uint z) {
         z = add(mul(x, y), RAY / 2) / RAY;
     }
-    function wdiv(uint x, uint y) internal returns (uint z) {
+    function wdiv(uint x, uint y) internal pure returns (uint z) {
         z = add(mul(x, WAD), y / 2) / y;
     }
-    function rdiv(uint x, uint y) internal returns (uint z) {
+    function rdiv(uint x, uint y) internal pure returns (uint z) {
         z = add(mul(x, RAY), y / 2) / y;
     }
 
@@ -66,7 +66,7 @@ contract DSMath {
     //  Also, EVM division is flooring and
     //    floor[(n-1) / 2] = floor[n / 2].
     //
-    function rpow(uint x, uint n) internal returns (uint z) {
+    function rpow(uint x, uint n) internal pure returns (uint z) {
         z = n % 2 != 0 ? x : RAY;
 
         for (n /= 2; n != 0; n /= 2) {
--- src/math.t.sol
+++ src/math.t.sol
@@ -13,88 +13,88 @@ import "ds-test/test.sol";
 import "./math.sol";
 
 contract DSMathTest is DSTest, DSMath {
-    function testFail_add() {
+    function testFail_add() public pure {
         add(2 ** 256 - 1, 1);
     }
-    function test_add() {
+    function test_add() public {
         assertEq(add(0, 0), 0);
         assertEq(add(0, 1), 1);
         assertEq(add(1, 1), 2);
     }
 
-    function testFail_sub() {
+    function testFail_sub() public pure {
         sub(0, 1);
     }
-    function test_sub() {
+    function test_sub() public {
         assertEq(sub(0, 0), 0);
         assertEq(sub(1, 1), 0);
         assertEq(sub(2, 1), 1);
     }
 
-    function testFail_mul() {
+    function testFail_mul() public pure {
         mul(2 ** 254, 6);
     }
 
-    function test_mul() {
+    function test_mul() public {
         assertEq(mul(0, 1), 0);
         assertEq(mul(1, 1), 1);
         assertEq(mul(2, 1), 2);
     }
 
-    function test_min() {
+    function test_min() public {
         assertEq(min(1, 1), 1);
         assertEq(min(1, 2), 1);
     }
-    function test_max() {
+    function test_max() public {
         assertEq(max(1, 1), 1);
         assertEq(max(1, 2), 2);
     }
-    function test_imin() {
+    function test_imin() public {
         assertEq(imin(1, 1), 1);
         assertEq(imin(1, 2), 1);
         assertEq(imin(1, -2), -2);
     }
-    function test_imax() {
+    function test_imax() public {
         assertEq(imax(1, 1), 1);
         assertEq(imax(1, 2), 2);
         assertEq(imax(1, -2), 1);
     }
 
-    function testFail_wmul_overflow() {
+    function testFail_wmul_overflow() public pure {
         wmul(2 ** 128, 2 ** 128);
     }
-    function test_wmul_trivial() {
+    function test_wmul_trivial() public {
         assertEq(wmul(2 ** 128 - 1, 1.0 ether), 2 ** 128 - 1);
         assertEq(wmul(0.0 ether, 0.0 ether), 0.0 ether);
         assertEq(wmul(0.0 ether, 1.0 ether), 0.0 ether);
         assertEq(wmul(1.0 ether, 0.0 ether), 0.0 ether);
         assertEq(wmul(1.0 ether, 1.0 ether), 1.0 ether);
     }
-    function test_wmul_fractions() {
+    function test_wmul_fractions() public {
         assertEq(wmul(1.0 ether, 0.2 ether), 0.2 ether);
         assertEq(wmul(2.0 ether, 0.2 ether), 0.4 ether);
     }
 
-    function testFail_wdiv_zero() {
+    function testFail_wdiv_zero() public pure {
         wdiv(1.0 ether, 0.0 ether);
     }
-    function test_wdiv_trivial() {
+    function test_wdiv_trivial() public {
         assertEq(wdiv(0.0 ether, 1.0 ether), 0.0 ether);
         assertEq(wdiv(1.0 ether, 1.0 ether), 1.0 ether);
     }
-    function test_wdiv_fractions() {
+    function test_wdiv_fractions() public {
         assertEq(wdiv(1.0 ether, 2.0 ether), 0.5 ether);
         assertEq(wdiv(2.0 ether, 2.0 ether), 1.0 ether);
     }
 
-    function test_wmul_rounding() {
+    function test_wmul_rounding() public {
         uint a = .950000000000005647 ether;
         uint b = .000000001 ether;
         uint c = .00000000095 ether;
         assertEq(wmul(a, b), c);
         assertEq(wmul(b, a), c);
     }
-    function test_rmul_rounding() {
+    function test_rmul_rounding() public {
         uint a = 1 ether;
         uint b = .95 ether * 10**9 + 5647;
         uint c = .95 ether;
