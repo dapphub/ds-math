00797832ced0012060a645567c8a33f9eb15354e
rain <rainbreak@riseup.net>
2018-11-23 03:47:34 +0000

    be consistent with community underflow usage

--- src/math.sol
+++ src/math.sol
@@ -20,7 +20,7 @@ contract DSMath {
         require((z = x + y) >= x, "ds-math-add-overflow");
     }
     function sub(uint x, uint y) internal pure returns (uint z) {
-        require((z = x - y) <= x, "ds-math-sub-overflow");
+        require((z = x - y) <= x, "ds-math-sub-underflow");
     }
     function mul(uint x, uint y) internal pure returns (uint z) {
         require(y == 0 || (z = x * y) / y == x, "ds-math-mul-overflow");
