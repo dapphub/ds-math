312ea50efbae8e4f1d152f804237166cdb571abf
CodingYourLife <johannes.mayer@conda.at>
2018-11-29 18:23:47 +0100

    added tests for all functions of DSMathTest. note that one function fails even tough the name suggests it should not CRITICAL

--- test/math.t.test.js
+++ test/math.t.test.js
@@ -11,4 +11,68 @@ contract('DSMathTest', async (accounts) => {
     it('testFail_add()', async() => {
         await shouldFail.reverting(math.testFail_add())
     })
+
+    it('testFail_sub()', async() => {
+        await shouldFail.reverting(math.testFail_sub())
+    })
+
+    it('test_sub()', async() => {
+        await math.test_sub()
+    })
+
+    it('testFail_mul()', async() => {
+        await shouldFail.reverting(math.testFail_mul())
+    })
+
+    it('test_mul()', async() => {
+        await math.test_mul()
+    })
+
+    it('test_min()', async() => {
+        await math.test_min()
+    })
+
+    it('test_max()', async() => {
+        await math.test_max()
+    })
+
+    it('test_imin()', async() => {
+        await math.test_imin()
+    })
+
+    it('test_imax()', async() => {
+        await math.test_imax()
+    })
+
+    it('testFail_wmul_overflow()', async() => {
+        await shouldFail.reverting(math.testFail_wmul_overflow())
+    })
+
+    it('test_wmul_trivial()', async() => {
+        await math.test_wmul_trivial()
+    })
+
+    it('test_wmul_fractions()', async() => {
+        await math.test_wmul_fractions()
+    })
+
+    it('testFail_wdiv_zero()', async() => {
+        await shouldFail.reverting(math.testFail_wdiv_zero())
+    })
+
+    it('test_wdiv_trivial()', async() => {
+        await math.test_wdiv_trivial()
+    })
+
+    it('test_wdiv_fractions()', async() => {
+        await math.test_wdiv_fractions()
+    })
+
+    it('test_wmul_rounding()', async() => {
+        await math.test_wmul_rounding()
+    })
+
+    it('test_rmul_rounding()', async() => {
+        await math.test_rmul_rounding()
+    })
 })
