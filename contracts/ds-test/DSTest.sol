8dfab26cdaefa58d80955cd8f813fa806f79c7bc
CodingYourLife <johannes.mayer@conda.at>
2018-11-29 18:27:35 +0100

    increased all pragmas to newer version

--- contracts/ds-test/DSTest.sol
+++ contracts/ds-test/DSTest.sol
@@ -11,7 +11,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity >0.4.23;
+pragma solidity 0.4.24;
 
 contract DSTest {
     event eventListener          (address target, bool exact);
