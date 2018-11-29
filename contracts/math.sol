2d017bc417e49405da4ba294f75eb069bc391535
CodingYourLife <johannes.mayer@conda.at>
2018-11-29 18:21:03 +0100

    changed all functions from internal to public (no security risk and first step for library)

--- contracts/math.sol
+++ contracts/math.sol
@@ -16,42 +16,42 @@
 pragma solidity >0.4.13;
 
 contract DSMath {
-    function add(uint x, uint y) internal pure returns (uint z) {
+    function add(uint x, uint y) public pure returns (uint z) {
         require((z = x + y) >= x, "ds-math-add-overflow");
     }
-    function sub(uint x, uint y) internal pure returns (uint z) {
+    function sub(uint x, uint y) public pure returns (uint z) {
         require((z = x - y) <= x, "ds-math-sub-underflow");
     }
-    function mul(uint x, uint y) internal pure returns (uint z) {
+    function mul(uint x, uint y) public pure returns (uint z) {
         require(y == 0 || (z = x * y) / y == x, "ds-math-mul-overflow");
     }
 
-    function min(uint x, uint y) internal pure returns (uint z) {
+    function min(uint x, uint y) public pure returns (uint z) {
         return x <= y ? x : y;
     }
-    function max(uint x, uint y) internal pure returns (uint z) {
+    function max(uint x, uint y) public pure returns (uint z) {
         return x >= y ? x : y;
     }
-    function imin(int x, int y) internal pure returns (int z) {
+    function imin(int x, int y) public pure returns (int z) {
         return x <= y ? x : y;
     }
-    function imax(int x, int y) internal pure returns (int z) {
+    function imax(int x, int y) public pure returns (int z) {
         return x >= y ? x : y;
     }
 
     uint constant WAD = 10 ** 18;
     uint constant RAY = 10 ** 27;
 
-    function wmul(uint x, uint y) internal pure returns (uint z) {
+    function wmul(uint x, uint y) public pure returns (uint z) {
         z = add(mul(x, y), WAD / 2) / WAD;
     }
-    function rmul(uint x, uint y) internal pure returns (uint z) {
+    function rmul(uint x, uint y) public pure returns (uint z) {
         z = add(mul(x, y), RAY / 2) / RAY;
     }
-    function wdiv(uint x, uint y) internal pure returns (uint z) {
+    function wdiv(uint x, uint y) public pure returns (uint z) {
         z = add(mul(x, WAD), y / 2) / y;
     }
-    function rdiv(uint x, uint y) internal pure returns (uint z) {
+    function rdiv(uint x, uint y) public pure returns (uint z) {
         z = add(mul(x, RAY), y / 2) / y;
     }
 
@@ -70,7 +70,7 @@ contract DSMath {
     //  Also, EVM division is flooring and
     //    floor[(n-1) / 2] = floor[n / 2].
     //
-    function rpow(uint x, uint n) internal pure returns (uint z) {
+    function rpow(uint x, uint n) public pure returns (uint z) {
         z = n % 2 != 0 ? x : RAY;
 
         for (n /= 2; n != 0; n /= 2) {
