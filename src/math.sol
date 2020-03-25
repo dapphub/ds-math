406a81ee337ff484f25b6b07a20a7b832de426c9
Martin Lundfall <martin.lundfall@protonmail.com>
2020-03-25 11:22:46 +0100

    Document rounding behaviour

--- src/math.sol
+++ src/math.sol
@@ -42,15 +42,19 @@ contract DSMath {
     uint constant WAD = 10 ** 18;
     uint constant RAY = 10 ** 27;
 
+    //rounds to zero if x*y < WAD / 2
     function wmul(uint x, uint y) internal pure returns (uint z) {
         z = add(mul(x, y), WAD / 2) / WAD;
     }
+    //rounds to zero if x*y < WAD / 2
     function rmul(uint x, uint y) internal pure returns (uint z) {
         z = add(mul(x, y), RAY / 2) / RAY;
     }
+    //rounds to zero if x*y < WAD / 2
     function wdiv(uint x, uint y) internal pure returns (uint z) {
         z = add(mul(x, WAD), y / 2) / y;
     }
+    //rounds to zero if x*y < RAY / 2
     function rdiv(uint x, uint y) internal pure returns (uint z) {
         z = add(mul(x, RAY), y / 2) / y;
     }
